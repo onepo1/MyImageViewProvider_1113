e442d9b85e35623e262d4664fd3e61a351f851ac
onepo1 <1po@korea.com>
2018-11-13 16:01:33 +0900

    카메라 가져오기

--- app/src/main/java/com/example/edu/myimageviewprovider_1113/MainActivity.java
+++ app/src/main/java/com/example/edu/myimageviewprovider_1113/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.edu.myimageviewprovider_1113;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -9,13 +10,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.provider.MediaStore;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int LOAD_IMAGE =101;
-    Button fromGalleryButton;
+    int IMAGE_CAPTURE = 102;
+    Button fromGalleryButton, imageCaptureButton;
     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +26,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         fromGalleryButton = (Button)findViewById(R.id.fromGalleryButton);
         fromGalleryButton.setOnClickListener(this);
+        imageCaptureButton = (Button)findViewById(R.id.imageCaptureButton);
+        imageCaptureButton.setOnClickListener(this);
         imageView = (ImageView)findViewById(R.id.imagesViewFromGallery);
     }
 //
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent,LOAD_IMAGE);
+
+        switch (v.getId()){
+            case R.id.fromGalleryButton:
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,LOAD_IMAGE);
+                break;
+
+            case R.id.imageCaptureButton:
+                if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, IMAGE_CAPTURE);
+                }
+                break;
+        }
+
+
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(data !=null){
-            Uri selectedImage = data.getData();
-            InputStream inputStream = null;
-            try {
-                inputStream = this.getContentResolver().openInputStream(selectedImage);
-                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                imageView.setImageBitmap(bitmap);
+        if(requestCode == LOAD_IMAGE && resultCode == RESULT_OK ) {
+            if(data !=null){
+                Uri selectedImage = data.getData();
+                InputStream inputStream = null;
+                try {
+                    inputStream = this.getContentResolver().openInputStream(selectedImage);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(bitmap);
 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
             }
+        }
 
 
+        if(requestCode == IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
         }
     }
 }
